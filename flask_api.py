7abb2053198c7d9fdfb030098c6b6aa53fe5a912
priya-singh2411 <priya2411.singh@gmail.com>
2020-10-03 20:32:52 +0530

    Add files via upload

--- flask_api.py
+++ flask_api.py
@@ -1,16 +1,19 @@
 # -*- coding: utf-8 -*-
 """
-Created on Fri Oct  2 20:21:05 2020
+Created on Sat Oct  3 19:41:18 2020
 
-@author: priya singh
+@author: priya Singh
 """
 #import libraries
 import pickle
 from flask import Flask, request
+#import flasgger
+from flasgger import Swagger
 #import numpy as np
 import pandas as pd
 
 app=Flask(__name__)
+Swagger(app)
 
 #Unpickling the classifier
 pickle_in=open('classifier.pkl','rb')
@@ -21,8 +24,34 @@ classifier=pickle.load(pickle_in)
 def welcome_page():
     return '<h1> WELCOME TO THE HOMEPAGE </h1>'
 
-@app.route('/predict')
+@app.route('/predict',methods=["GET"])
 def predict_note_authentication():
+    
+    """ Let's Authenticate the Bank Note 
+    This is using docstring for specification.
+    ---
+    parameters:
+      - name: variance
+        in: query
+        type: number
+        required: true
+      - name: skewness
+        in: query
+        type: number
+        required: true
+      - name: curtosis
+        in: query
+        type: number
+        required: true
+      - name: entropy
+        in: query
+        type: number
+        required: true
+    responses:
+        200:
+            description: The output values         
+    
+    """
     variance=request.args.get('variance')
     skewness=request.args.get('skewness')
     curtosis=request.args.get('curtosis')
@@ -32,14 +61,30 @@ def predict_note_authentication():
 
 @app.route('/predict_file',methods=["POST"])
 def predict_note_file():
-        df_test=pd.read_csv(request.files.get("file"))
-        print(df_test.head())
-        prediction=classifier.predict(df_test)
-        return 'Predicted value for the CSV is : ' +str(list(prediction))
+    
+    """ Let's Authenticate the Bank Note 
+    This is using docstring for specification.
+    ---
+    parameters:
+      - name: file
+        in: formData
+        type: file
+        required: true
+    responses:
+        200:
+            description: The output values         
+    
+    """
+    df_test=pd.read_csv(request.files.get("file"))
+    print(df_test.head())
+    prediction=classifier.predict(df_test)
+    return str(list(prediction))
     
       
 
 
 if __name__=='__main__':
     app.run(debug=True)
+
+#url to run the app : http://127.0.0.1:5000/apidocs/
 
