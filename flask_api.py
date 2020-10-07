e2d39e7b7111c987bc5753a57c18ae53dc973cb2
priya-singh2411 <priya2411.singh@gmail.com>
2020-10-07 21:13:25 +0530

    Add files via upload

--- flask_api.py
+++ flask_api.py
@@ -19,8 +19,8 @@ Swagger(app)
 pickle_in=open('classifier.pkl','rb')
 classifier=pickle.load(pickle_in)
 
-@app.route('/')
-@app.route('/home')
+#@app.route('/')
+@app.route('/apidocs')
 def welcome_page():
     return '<h1> WELCOME TO THE HOMEPAGE </h1>'
 
